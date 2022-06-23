1ae087058757c6d24b5bb41998ceb047804d734e
Benjamin <bstanf@uw.edu>
2022-06-22 18:31:20 -0700

    adding more psuedo-code.

---
+++ script.js
@@ -0,0 +1,18 @@
+// declare currentTime
+// declare currentDayEl
+// declare timeEntriesEl
+
+// single event listener to the parent to bubble-up for the save button
+
+// render a block for each hour of the day.
+
+// forloop with a templateLiteral for the html for the time blocks
+// for(var i = 9; i <= 17; i++) {
+    // Attempt to get the saved data for the hour of the loop. 
+// Template literal for the timeblocks and save buttons go here.
+
+// determine if the hour of the loop is before, current, or future and apply unique css for each.
+
+// }
+
+// save an hour to local storage (this is tied to the save button listener.)
